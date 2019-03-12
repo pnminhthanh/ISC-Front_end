2f13e42253d6da33b7a6f4178a72aeeaa24f1b70
Minh Thanh <mix.kris@gmail.com>
2019-03-12 12:09:18 +0700

    Sua ten Subject thanh SubjectInterface trong Classes Service

--- src/app/services/classes.service.ts
+++ src/app/services/classes.service.ts
@@ -3,27 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
 import { Course } from './course.service';
-import { Subject } from './subject.service';
+import { SubjectInterface } from './subject.service';
 
 export interface ClassesRespone{
-  errorCode:number;
-  data:Classes;
-  Message:string;
+  errorCode: number;
+  data: Classes;
+  Message: string;
 }
 export interface ClassessRespone{
-  errorCode:number;
-  data:Classes [];
-  Message:string;
+  errorCode: number;
+  data: Classes [];
+  Message: string;
 }
 export interface Classes{
-  id:number;
-  courseid:number;
-  subjectid:number;
-  percentBan:number;
-  passingscore:number;
+  id: number;
+  courseid: number;
+  subjectid: number;
+  percentBan: number;
+  passingscore: number;
   name: string;
-  course : Course;
-  subject : Subject;
+  course: Course;
+  subject: SubjectInterface;
 }
 
 @Injectable({
@@ -31,15 +31,15 @@ export interface Classes{
 })
 export class ClassesService {
 
-  constructor(private http:HttpClient, private api:ApiService) { }
+  constructor(private http: HttpClient, private api: ApiService) { }
 
-  getall():Observable<ClassessRespone>{
+  getall(): Observable<ClassessRespone>{
     return this.http.get<ClassessRespone>(this.api.apiUrl.classes);
   }
-  getone(id):Observable<ClassesRespone>{
+  getone(id): Observable<ClassesRespone>{
     return this.http.get<ClassesRespone>(this.api.apiUrl.classes + '/' + id);
   }
-  get(id):Observable<ClassessRespone>{
+  get(id): Observable<ClassessRespone>{
     return this.http.get<ClassessRespone>(this.api.apiUrl.classes + '/' + id);
   }
 }
